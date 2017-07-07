44ee7261104a1500ba4867e77c18c2aee518129b
linyoulin <546407078@qq.com>
2017-07-07 08:25:35 +0800

    no message

--- consumer/src/main/java/com/linyoulin/dubbotest/client/consumerClient.java
+++ consumer/src/main/java/com/linyoulin/dubbotest/client/consumerClient.java
@@ -1,10 +1,17 @@
 package com.linyoulin.dubbotest.client;
 
+import com.linyoulin.dubbotest.service.IHelloService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by linyoulin on 2017/7/5.
  */
 public class ConsumerClient {
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
+        IHelloService helloService = (IHelloService) context.getBean("helloService");
+        String hello = helloService.sayHello("linlin");
+        System.out.println(hello);
     }
 }
